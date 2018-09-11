876f522a6e2e532a664958cfb162978e164ccc18
xar8 <jevans42@ecc-jaguar.csuchico.edu>
2018-09-10 17:30:59 -0700

    Added biggest variable and output statement.

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -6,6 +6,7 @@ int main()
   //declare variables here
   int number1;
   int number2;
+  int biggest;
 
   std::cout<<"Please enter a whole number:\n";
   //get first input using std::cin>>
@@ -14,11 +15,19 @@ int main()
   //get second input using std::cin>> again with a second variable
   std::cin>> number2;
   //determine if first or second is bigger and store that in a third variable
-
-
-  std::cout<<"Of those two numbers, the biggest is: ";
+  if (number1 > number2) 
+       { 
+        biggest = number1;
+       }
+  else if (number2 > number1) 
+       {
+         biggest = number2;
+       } 
+      
+  std::cout<<"Of those two numbers, the biggest is: " << biggest <<std::endl;
+     
   //print out the variable representing the biggest number
-
+  
   std::cout<<std::endl<<"Thank you for playing.\n";
   
   return 0;
